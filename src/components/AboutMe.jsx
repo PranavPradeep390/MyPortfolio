6f152c24c7e3b7104c6880337ced6474359e95cd
Pranav Pradeep <pranavpradeep786@gmail.com>
2024-07-18 20:56:22 +0530

    aboutme completed

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -3,6 +3,7 @@ import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import InstagramIcon from '@mui/icons-material/Instagram';
+import aboutpic from "../Assets/aboutpic.png"
 
 function AboutMe() {
 
@@ -23,12 +24,14 @@ function AboutMe() {
         </div>
         <div className='flex flex-wrap gap-14 p-2 md:p-14 mt-10 md:mt-0 md:items-center'>
             <div className='w-full md:w-[40%]'>
-                <img className='mx-auto w-[50%] md:w-full md:h-full rounded-[40%] md:rounded-none' src="https://kaleidoscopic-pie-2341a3.netlify.app/template/img/info-img.jpg" alt="NA" />
+                <img className='mx-auto w-[50%] md:w-full md:h-full rounded-[40%] md:rounded-none md:rounded-b-full' src={aboutpic} alt="NA" />
             </div>
             <div className='w-full md:w-[55%]'>
-                <p className='text-[#009e66] font-normal text-[18px] md:text-[25px] font_main mb-4'>Who am i?</p>
-                <p className='font-bold font_main text-[20px] md:text-[30px] mb-4'>I'm Pranav Pradeep, a visual UX/UI Designer and Web Developer</p>
-                <p className='text-[#7a7878] font_main leading-7 text-[14px] font-medium'>I am a freelancer based in the United Kingdom and i have been building noteworthy UX/UI designs and websites for years, which comply with the latest design trends. I help convert a vision and an idea into meaningful and useful products. Having a sharp eye for product evolution helps me prioritize tasks, iterate fast and deliver faster.</p>
+                <p className='text-[#009e66] font-normal text-[18px] md:text-[25px] font_main mb-4'>Who am i ?</p>
+                <p className='font-bold font_main text-[20px] md:text-[30px] mb-4'>I'm Pranav Pradeep, Full Stack Developer and Designer</p>
+                <p className='text-[#7a7878] font_main leading-7 text-[14px] font-medium'>
+                I am a full stack developer from Ernakulam, and I have been building noteworthy UX/UI designs and websites that comply with the latest design trends. I help convert a vision and an idea into meaningful and useful products. Having a sharp eye for product evolution helps me prioritize tasks, iterate quickly, and deliver faster.
+                </p>
 
                 <hr className='border-1 border-[#009e66] my-8' />
 
@@ -53,8 +56,8 @@ function AboutMe() {
                 <div>
                 <a href='' target='_blank'><FacebookRoundedIcon className='mb-1 me-2  text-[#7a7878]'/></a>
                 <a href='' target='_blank'><WhatsAppIcon className='mb-1 me-2 text-[#7a7878]'/></a>
-                <a href='' target='_blank'><LinkedInIcon className='mb-1 me-2 text-[#7a7878]'/></a>
-                <a href='' target='_blank'><InstagramIcon className='mb-1 text-[#7a7878]'/></a>
+                <a href='https://www.linkedin.com/in/pranav-pradeep-8b11a8283/' target='_blank'><LinkedInIcon className='mb-1 me-2 text-[#7a7878]'/></a>
+                <a href='https://www.instagram.com/im_pranavpradeep/' target='_blank'><InstagramIcon className='mb-1 text-[#7a7878]'/></a>
        
                 </div>
                 </div>
